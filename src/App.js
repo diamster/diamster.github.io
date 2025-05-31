303ceae9e6f7d3fe3feae51b57a16ab9c6afbe17
Ryan <ryf@sfu.ca>
2025-05-30 18:29:25 -0700

    chore: added react setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import RyanBio from "./RyanBio";
+
+function App() {
+  return (
+    <>
+      <p>Hello World!</p>
+      <RyanBio></RyanBio>
+    </>
+  );
+}
+
+export default App;
---
+++ src/RyanBio.js
@@ -0,0 +1,12 @@
+function RyanBio() {
+    return (
+        <div>
+            <h1>Ryan</h1>
+            <p>I am hungry</p>
+        </div>
+
+
+    );
+}
+
+export default RyanBio;
