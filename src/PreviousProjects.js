f9b7858151e7d54bc1669c751b1499785c6f8841
Ryan <ryf@sfu.ca>
2025-06-09 14:58:12 -0700

    feat: Added a photo/screenshot of Fario in the previous projects section

--- src/PreviousProjects.js
+++ src/PreviousProjects.js
@@ -1,14 +1,16 @@
+import photo from "./images/FarioGameplayScreenshot.png";
 function PreviousProjects() {
     return (
         <div className="previous-projects-section">
             <h2 className="project-headings">Previous Projects</h2>
             <div className="previous-projects-items">
                 <h3 className="project-title">Fario (Unity Game)</h3>
+                <img src={photo} alt="Gameplay screenshot of Fario" className="game-image"></img>
                 <p className="project-description">
                     A 2D platformer built in the Unity Game Engine featuring health and shield collectibles, enemies, and multiple levels.
                 </p>
                 <ul className="project-details">
-                    <li><strong>Start Date:</strong> January 2024</li>
+                    <li><strong>Start Date:</strong> January 2023</li>
                     <li><strong>End Date:</strong> April 2023</li>
                     <li><strong>Status:</strong> Completed</li>
                     <li><strong>Tech Stack:</strong> Unity Game Engine, C#</li>
