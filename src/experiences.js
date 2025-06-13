da678db6763da2a3536e9698961896e28d3bf7cf
Ryan <ryf@sfu.ca>
2025-06-13 13:02:11 -0700

    feat: Added a button at the bottom of experience page that takes user to project page

--- src/experiences.js
+++ src/experiences.js
@@ -1,11 +1,13 @@
 import WorkExperience from "./WorkExperience";
 import VolunteerExperience from "./VolunteerExperience";
+import ProjectButton from "./ProjectButton";
 
 function Experiences () {
     return (
         <div className="page-setup">
             <WorkExperience></WorkExperience>
             <VolunteerExperience></VolunteerExperience>
+            <ProjectButton></ProjectButton>
         </div>
     );
 }
