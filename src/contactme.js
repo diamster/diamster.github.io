c110d7a93c262758b0acdc13bf01d3e01f0f12ab
Ryan <ryf@sfu.ca>
2025-06-16 14:39:51 -0700

    feat: Changed styles CSS with updated colours and clean up to code

--- src/contactme.js
+++ src/contactme.js
@@ -3,6 +3,7 @@ function ContactMe() {
         <div className="contact-page">
             <div className="contact-box">
                 <h2>Contact Me</h2>
+                <p>If you'd like to get in touch about work opportunities, collaborations, or just want to say hello, feel free to reach out to me on these sites!</p>
                 <p><strong>Email:</strong> ryf@sfu.ca</p>
                 <p><strong>Connect with me:</strong></p>
         <ul className="contact-links">
@@ -11,7 +12,7 @@ function ContactMe() {
         </ul>
 
         <p>
-          <strong>Resume:</strong> <a href="/files/Ryan_Fu_Resume.pdf" download className="external-button">Download my resume!</a>
+          <strong>Resume:</strong> <a href="/files/Ryan_Fu_Resume.pdf" download className="external-button">Download a copy of my resume!</a>
         </p>
 
         <p>Thank you for checking out my website!</p>
